601efd76bc1fc424ff1abd1567ed4371f6129b60
Rekran <arthur.ricardo17@gmail.com>
2024-05-15 23:09:38 -0300

    .

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,7 @@
 from fastapi import FastAPI
-from models import Base
-from database import engine
 import uvicorn
-
+from backend.modules.user.routes import router
 
 app = FastAPI()
 
-
-from routes import routerCsv
-
-app.include_router(routerCsv)
-
-if __name__ == "__main__":
-    Base.metadata.create_all(bind=engine)
-    
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+app.include_router(router)
